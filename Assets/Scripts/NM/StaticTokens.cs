933c16c6a6b68787881d713093a2d774461b8f5a
Ronnie Rawlings <rr258736@falmouth.ac.uk>
2023-07-19 21:14:41 +0100

    Implemented - Player Starting Tokens
    This commit introduces the players starting tokens and a way to access them. A visual representation of them has also been placed next to each players hand.
    
    Next, implement a way to have each CPU player auto place a token at the beginning of play and pick a horse for their 2nd token.

---
+++ Assets/Scripts/NM/StaticTokens.cs
@@ -0,0 +1,11 @@
+// Author - Ronnie Rawlings.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class StaticTokens
+{
+    // Players current tokens, used for entering play.
+    public static int playerOneTokens = 10, playerTwoTokens = 10, playerThreeTokens = 10, playerFourTokens = 10;
+}
