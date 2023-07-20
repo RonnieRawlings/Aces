12e81e3acddf4525b05e0202de4ff5faefa6af3f
Ronnie Rawlings <rr258736@falmouth.ac.uk>
2023-07-20 13:31:26 +0100

    Implemented - AI Token Placement
    This commit implements functionality which allows the CPU players to place their middle tokens at the start of the game and pick a random horse to place tokens on.
    
    Next, allow the player to place their own tokens.

---
+++ Assets/Scripts/NM/CPUBehaviour.cs
@@ -0,0 +1,35 @@
+// Author - Ronnie Rawlings.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CPUBehaviour : MonoBehaviour
+{
+    // Places where player can place their tokens.
+    [SerializeField] private GameObject middleToken;
+    [SerializeField] private List<GameObject> horseTokens;
+
+    // Players starting tokens;
+    [SerializeField] private int playerTokens = 10;
+
+    /// <summary> method <c>StartingPlay</c> Starts the CPUs play, places starting tokens + chooses a horse. </summary>
+    public void StartingPlay()
+    {
+        playerTokens = playerTokens - 2;
+        middleToken.SetActive(true);
+        horseTokens[Random.Range(0, 4)].SetActive(true);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartingPlay();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
