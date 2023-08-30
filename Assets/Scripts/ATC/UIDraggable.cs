5bf03b71c1dadb50d3a8264abeee0de120403bb4
Ronnie Rawlings <rr258736@falmouth.ac.uk>
2023-08-30 19:51:22 +0100

    Improved/Implemented - Clock Piles Pos + Rotation Reset
    This commit improves where the clock piles are positioned so the cards don't go off screen on lower resolutions. It also implements functionality to have each card the player drags be straight up instead of the position it's picked in.
    
    Next, build/package for v0.7.1 release.

--- Assets/Scripts/ATC/UIDraggable.cs
+++ Assets/Scripts/ATC/UIDraggable.cs
@@ -9,8 +9,13 @@ public class UIDraggable : MonoBehaviour, IDragHandler
 {
     public void OnDrag(PointerEventData eventData)
     {
+        if (transform.rotation.z != 0)
+        {
+            transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y, 0, transform.rotation.w);
+        }
         this.transform.position += (Vector3)eventData.delta;
     }
+
 
     private void Start()
     {
